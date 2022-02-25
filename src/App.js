45abfbcdd8f8c84fdce43e004d74f25e72ee95b1
Renildo15 <renildorabi22@gmail.com>
2022-02-25 17:59:15 -0300

    Finalizada

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+import Calculadora from './Components/Calculadora';
+import './App.css'
+
+export default function App() {
+ 
+  return (
+  
+    <div className='App'>
+        <Calculadora/>
+    </div>
+    
+  );
+}
+
+
